86037cbee877736a42406ad26109906d11b5186e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-18 15:15:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,4 +12,4 @@ const newID = oldID + 1000000000
 const ageIsValid = Number.isInteger(currentAge)
 const randomNumber = Math.random()
 const randomInteger = Math.floor(randomNumber)
-const randomUserID = 
+const randomUserID = randomInteger + 1000000001 
